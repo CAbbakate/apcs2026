64addd8414cc1a9903d7abfcdfc275070e08aed3
Lincoln L Ribeiro Da Costa <LLRibeiroDaCosta@stu.naperville203.org>
2026-09-22 14:10:54 -0500

    ticket class

---
+++ Chapter2/TextTransformer/Text.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Text{
+    public static void Main(String[] args){
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Welcome to IURHUIETAET OIO:IAET IJ,");
+        
+        System.out.println("\n?  👁 ??\n");
+        String user = input.nextLine();
+        
+        System.out.println("\n\n\n\nrun.\n\n");
+        
+        
+        
+        
+        
+        
+        
+        input.nextLine();
+        input.nextLine();
+        input.nextLine();
+        System.out.println("Stop searching.");
+        input.nextLine();
+        input.nextLine();
+        input.nextLine();
+        
+        System.out.println("\nJava.lang ArithmicExpression Error: Divison by 0\n");
+    }
+}
